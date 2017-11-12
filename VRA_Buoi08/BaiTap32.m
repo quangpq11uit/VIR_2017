5c0b63495072ffad1389bbe27966f1e877fb3feb
Quang Phan <quangpq.11@grad.uit.edu.vn>
2017-11-12 15:10:05 +0700

    Lưu kết quả rút trích features

--- VRA_Buoi08/BaiTap32.m
+++ VRA_Buoi08/BaiTap32.m
@@ -3,24 +3,19 @@ function BaiTap32()
      rootFolderTrain = fullfile('../VRA_Buoi07/DataTrain');
      category = {'0','1','2','3','4','5','6','7','8','9'};
      imdsDataTrain = imageDatastore(fullfile(rootFolderTrain, category), 'LabelSource', 'foldernames');
-     imdsDataTrain.ReadFcn = @(filename)readAndPreprocessImage(filename);
      
-     net = alexnet();
-     featureLayer = 'fc7';
-     
-     featuresDataTrain = activations(net, imdsDataTrain, featureLayer, 'MiniBatchSize', 32, 'OutputAs', 'columns');
+     featuresDataTrain = loadFeatures('featuresTrain.mat', 'features');
      lblDataTrain = imdsDataTrain.Labels;
      
      classifier = fitcdiscr(featuresDataTrain, lblDataTrain);
      
      rootFolderTest = fullfile('../VRA_Buoi07/DataTest');
      imdsDataTest = imageDatastore(fullfile(rootFolderTest, category), 'LabelSource', 'foldernames');
-     imdsDataTest.ReadFcn = @(filename)readAndPreprocessImage(filename);
      
-     featuresDataTest = activations(net, imdsDataTest, featureLayer, 'MiniBatchSize', 32, 'OutputAs', 'columns');
+     featuresDataTest = loadFeatures('featuresTest.mat', 'features');
      lblDataTest = imdsDataTest.Labels;
      
-     lblResult = predict(classifier, featuresDataTest);
+     lblResult = predict(classifier, featuresDataTest');
      
      nResult = (lblDataTest == lblResult);
      nCount = sum(nResult);
